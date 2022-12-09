e213eb3bd76b301782dcaceb60a12569b7a34118
sewon <kimsewon0076@gmail.com>
2022-12-09 18:01:50 +0900

    20221209

---
+++ db.js
@@ -0,0 +1,57 @@
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "seomoon_notice",
+});
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected!");
+});
+//메모를 추출할때
+function getNotice(callback) {
+    connection.query("SELECT * FROM seomoon_db ORDER BY id desc", (err, rows) => {
+        if (err) throw err;
+        callback(rows);
+    });
+}
+//메모를 입력할때
+function insertNotice(title, name, content, callback) {
+    connection.query(`insert into seomoon_db (create_time, name,title,content) values (NOW(), '${name}','${title}','${content}')`, (err) => {
+        if (err) throw err;
+        callback();
+    });
+}
+//메모중 아이디가 일치하는 데이터 추출(내보내는 건 못함)
+//아이디는 프라이머리 키로 공통된 숫자 불가능
+function getNoticeByid(id, callback) {
+    //한줄을 다 불러올때는 from + 'table 이름" + 없음
+    connection.query(`SELECT * FROM seomoon_db where id=${id}`, (err, row) => {
+        if (err) throw err;
+        callback(row);
+    });
+}
+//아이디가 일치하는 부분을 update한 내용 내보내기
+function updateNotice(id, title, name, content, callback) {
+    connection.query(`UPDATE seomoon_db set create_time=now(), name='${name}',title='${title}',content='${content}' where id=${id}`, (err) => {
+        if (err) throw err;
+        callback();
+    });
+}
+//아이디가 일치하면 삭제하기
+function deleteNotice(id, callback) {
+    connection.query(`DELETE from seomoon_db WHERE id=${id}`, (err) => {
+        if (err) throw err;
+        callback();
+    });
+}
+
+module.exports = {
+    //여러 함수가 다 들어갈 수 있다
+    getNotice,
+    insertNotice,
+    getNoticeByid,
+    updateNotice,
+    deleteNotice,
+};
--- routes/route.js
+++ routes/route.js
@@ -1,16 +1,20 @@
 //page 연결이 쉬워지도록
 //
-const { json } = require("body-parser");
-const { Domain } = require("domain");
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const fs = require("fs");
 const path = require("path");
+const db = require("./../db.js");
 
 router.get("/", (req, res) => {
     res.render("main");
 });
 router.get("/about_us", (req, res) => {
     res.render("about_us");
+});
+router.get("/store", (req, res) => {
+    res.render("store");
 });
 router.get("/login", (req, res) => {
     res.render("login");
@@ -37,130 +41,78 @@ router.post("/sign_up_info", (req, res) => {
     console.log(confimPw);
     console.log(phoneNum);
 });
+
+// ===========================================notice======================================
 router.get("/notice", (req, res) => {
-    res.render("notice");
+    db.getNotice((rows) => {
+        res.render("notice", { rows: rows });
+    });
 });
+//노티스 적는 페이지 받아오기
 router.get("/notice_write", (req, res) => {
     res.render("notice_write");
 });
+//노티스 적는 페이지의 폼 받아 노티스 리스트로 다시 보내기
+router.post("/notice_write_info", (req, res) => {
+    let param = JSON.parse(JSON.stringify(req.body));
+    let title = param["title"];
+    let name = param["userId"];
+    let content = param["content"];
+    db.insertNotice(title, name, content, () => {
+        res.redirect("/notice"); //redirect 에는 / 붙인다
+    });
+    //내가 원하는 페이지{내가 보낼것} 나는 notice_detail.ejs 페이지에 notice_write_info라는 이름으로 보낼거야
+    // res.render("memoList.ejs", { "writeMemo": param });
+    // res.render("notice.ejs", { "notice_write_info": param });
+});
 router.get("/notice_content", (req, res) => {
     res.render("notice_content");
+});
+//id에 맞는 내용 부르기
+router.get("/notice_detail", (req, res) => {
+    let id = req.query.id;
+
+    //getMemobyId는 그냥 지어준 이름
+    db.getNoticeByid(id, (row) => {
+        res.render("notice_content", { row: row[0] }); //테이블의 한 행만 보내줄거기 때문에
+    });
+});
+router.get("/updateNotice", (req, res) => {
+    let id = req.query.id;
+    console.log(id);
+    //getMemobyId는 그냥 지어준 이름
+    db.getNoticeByid(id, (row) => {
+        res.render("notice_rewrite", { row: row[0] });
+    });
+});
+//삭제하기======================================================
+router.get("/deleteN", (req, res) => {
+    let id = req.query.id;
+    db.deleteNotice(id, () => {
+        res.redirect("/notice");
+    });
+});
+
+// 업데이트 된 내용 내보내기=================================================
+router.post("/notice_rewrite", (req, res) => {
+    let param = JSON.parse(JSON.stringify(req.body));
+    let id = param["id"];
+    console.log("id 는" + id);
+    let title = param["title"];
+    let name = param["userId"];
+    let content = param["content"];
+    db.updateNotice(id, title, name, content, () => {
+        console.log("id 는" + id);
+        res.redirect("/notice");
+    });
 });
 router.get("/notice_write_result", (req, res) => {
     res.render("notice_write_result");
 });
 router.get("/notice_write_info", (req, res) => {
-    let id = param["userId"];
-    let pw = param["userPw"];
-    let title = param["title"];
-    res.render("notice_write_info");
-});
-router.post("/notice_write_info", (req, res) => {
-    let param = JSON.parse(JSON.stringify(req.body));
-    let id = param["userId"];
-    let pw = param["userPw"];
-    let title = param["title"];
-    let content = param["content"];
-    console.log(id);
-    console.log(pw);
-    console.log(title);
-    //내가 원하는 페이지로 보내는 역할
-    //내가 원하는 페이지{내가 보낼것} 나는 notice_detail.ejs 페이지에 notice_write_info라는 이름으로 보낼거야
-    res.render("notice_write_result.ejs", { "notice_write_info": param });
-    // res.render("notice.ejs", { "notice_write_info": param });
+    db.getNotice((rows) => {
+        res.render("notice_write", { rows: rows }); //ejs의 rows를 받아서 rows라는 이름으로 보낸다
+    });
 });
 
-// router.post('/notice_write_form',(req, res) => {
-//     let param = JSON.parse(JSON.stringify(req.body));
-//     let id = param['userId'];
-//     let title = param['title'];
-//     console.log(id);
-//     console.log(title);
-//     //내가 원하는 페이지로 보내는 역할
-//     //내가 원하는 페이지{내가 보낼것} 나는 viewer페이지에 writeMemo_emend라는 이름으로 보낼거야
-//     res.render('notice_write_form',{'notice_detail' : param})
-//   });
-// router.post('/result_page', function(req, res, next) {
-//     var id = req.body.userId;
-//     var age = req.body.title;
-//     console.log("## post request");
-//     res.render('result_page', { title: 'Express', id: id, title: title, method: "post" });
-// });
-// router.get("/community", (req, res) => {
-//     res.render("community");
-// });
-// router.post("/name", (req, res) => {
-//     let paramname = req.body.name;
-//     res.writeHead(200, { "content-type": "text/html;charset=utf-8" });
-//     res.write(`${paramname} 님 반갑습니다`);
-//     res.end();
-// });
-
-// router.post("/commuWrite", (req, res, next) => {
-//     let param = JSON.parse(JSON.stringify(req.body));
-//     //post는 parsing을 해줘야함,  get은 필요없음
-//     let id = param["id"];
-//     let title = param["title"];
-//     let content = param["content"];
-//     let pw = param["password"];
-
-//     console.log(id);
-//     console.log(title);
-//     console.log(content);
-//     console.log(pw);
-// });
-
-// // ========sign up .ejs==========seomoon==================
-// router.get("/sign_up", (req, res) => {
-//     res.render("sign_up");
-// });
-
-// router.post("/sign_up_form", (req, res) => {
-//     let paramS = JSON.parse(JSON.stringify(req.body));
-//     let id = paramS["id"];
-//     let pw = paramS["pw"];
-//     let pwC = paramS["pw_confirm"];
-//     let birth = paramS["birth"];
-//     console.log(id);
-//     console.log(pw);
-//     console.log(pwC);
-//     console.log(birth);
-// }),
-//게시판 리스트 페이지
-router.get("/notice_list", (req, res) => {
-    res.render("notice_list");
-});
-
-//게시판 리스트 뷰어 페이지
-router.get("/viewer", (req, res) => {
-    res.render("viewer");
-});
-
-//게시판 작성 페이지
-router.get("/notice_write", (req, res) => {
-    res.render("notice_write");
-});
-//게시판 수정 페이지
-router.get("/notice_write_emend", (req, res) => {
-    res.render("notice_write_emend");
-});
-//게시판 작성 페이지에서 작성하면 뷰어에서 나오도록 연결을 시켜주자!!!!!
-//제목 관리자 날짜 조회수 컨텐츠 순서로 기입
-//수정페이지
-router.post("/writeMemo_emend", (req, res) => {
-    let param = JSON.parse(JSON.stringify(req.body));
-    let write_list_emend_title = param["write_list_emend_title"];
-    let write_list_emend_name = param["write_list_emend_name"];
-    let write_list_emend_day = param["write_list_emend_day"];
-    let write_list_emend_view = param["write_list_emend_view"];
-    let write_emend_memo_cont = param["write_emend_memo_cont"];
-    console.log(write_list_emend_title);
-    console.log(write_list_emend_name);
-    console.log(write_list_emend_day);
-    console.log(write_list_emend_view);
-    console.log(write_emend_memo_cont);
-    //내가 원하는 페이지로 보내는 역할
-    //내가 원하는 페이지{내가 보낼것} 나는 viewer페이지에 writeMemo_emend라는 이름으로 보낼거야
-    res.render("viewer.ejs", { writeMemo_emend: param });
-});
 module.exports = router;
