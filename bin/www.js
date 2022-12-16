7e8aa1272e9556c2150aadc221c57edff44477fd
sewon <kimsewon0076@gmail.com>
2022-12-16 17:49:53 +0900

    www.js

--- bin/www.js
+++ bin/www.js
@@ -2,7 +2,7 @@
 let app = require("../app");
 let hostname = "127.0.0.1";
 let port = process.env.PORT || 10121;
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`Example app listening on port http://${hostname}:${port}/`);
     console.log(`${port}로 express 실행`);
 });
