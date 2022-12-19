992b2bbc76cb94b2779831ece75ffcf2a1d3e203
sewon <kimsewon0076@gmail.com>
2022-12-19 09:42:08 +0900

    hostname change

--- bin/www.js
+++ bin/www.js
@@ -1,6 +1,6 @@
 //실행시켜주는 애
 let app = require("../app");
-let hostname = "127.0.0.1";
+let hostname = "0.0.0.0";
 let port = process.env.PORT || 10121;
 app.listen(process.env.PORT || port, () => {
     console.log(`Example app listening on port http://${hostname}:${port}/`);
